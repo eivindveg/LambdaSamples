ac97266ad36a44110ac5cec018ae35a8c9799a41
eivindveg <eivindveg@hotmail.com>
2015-03-14 00:16:19 +0100

    Add simple javadoc explanation for SquareRootPowerAverager

--- src/no/westerdals/pg4100/lambdas/averager/SquareRootPowerAverager.java
+++ src/no/westerdals/pg4100/lambdas/averager/SquareRootPowerAverager.java
@@ -2,6 +2,10 @@ package no.westerdals.pg4100.lambdas.averager;
 
 import java.util.List;
 
+/**
+ * This interface is used to get the average value of numbers in a list,
+ * using first the square root of each value, then increasing the exponent to a given power.
+ */
 public interface SquareRootPowerAverager {
     public double getAverage(final List<? extends Number> numbers, final int power);
 }
