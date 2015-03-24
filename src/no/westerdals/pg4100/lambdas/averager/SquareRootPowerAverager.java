9ad61285fdbf165ac7840ec8bc060f1adb68c15f
eivindveg <eivindveg@hotmail.com>
2015-03-24 09:30:04 +0100

    Make interfaces functional(part 1 of 2)

--- src/no/westerdals/pg4100/lambdas/averager/SquareRootPowerAverager.java
+++ src/no/westerdals/pg4100/lambdas/averager/SquareRootPowerAverager.java
@@ -6,6 +6,7 @@ import java.util.List;
  * This interface is used to get the average value of numbers in a list,
  * using first the square root of each value, then increasing the exponent to a given power.
  */
+@FunctionalInterface
 public interface SquareRootPowerAverager {
     public double getAverage(final List<? extends Number> numbers, final int power);
 }
--- src/no/westerdals/pg4100/lambdas/minimum/MinimumIntegerExtractor.java
+++ src/no/westerdals/pg4100/lambdas/minimum/MinimumIntegerExtractor.java
@@ -2,6 +2,7 @@ package no.westerdals.pg4100.lambdas.minimum;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface MinimumIntegerExtractor {
     public int getMinimum(final List<Integer> integers);
 }
